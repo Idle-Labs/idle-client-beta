d1894765637ea61a37e236f9fb7d0b22d8fffa09
Samster <cester.samuele@gmail.com>
2020-08-06 17:36:15 +0200

    Updates

---
+++ precache-manifest.6cffe3518e2fdb8047ffd26b10d6d2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "945222c0fc2467afc74e",
+    "url": "/static/js/main.945222c0.chunk.js"
+  },
+  {
+    "revision": "e3254edd4911eef2a5c6",
+    "url": "/static/js/1.e3254edd.chunk.js"
+  },
+  {
+    "revision": "945222c0fc2467afc74e",
+    "url": "/static/css/main.ec764d93.chunk.css"
+  },
+  {
+    "revision": "e3254edd4911eef2a5c6",
+    "url": "/static/css/1.bd5df56d.chunk.css"
+  },
+  {
+    "revision": "d88ae237f53502da25ef1d42838107d3",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55cea80fcdde623a5fb1af85f9c1bf7e.js"
+  "/precache-manifest.6cffe3518e2fdb8047ffd26b10d6d2c6.js"
 );
 
 workbox.clientsClaim();
