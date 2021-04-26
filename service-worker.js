712bd2359b87148283db91e34d23d39748bb3c6d
Samster91 <cester.samuele@gmail.com>
2021-04-26 15:42:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e086aa30016d9f8b282389172cab7328.js"
+  "/precache-manifest.3c0debe35a654fa91772cb1a3bb087f2.js"
 );
 
 workbox.clientsClaim();
