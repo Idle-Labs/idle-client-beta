98b9358e66a949faedd3b93c6e2fe8df8f1f9523
Samster91 <cester.samuele@gmail.com>
2021-12-20 12:41:20 +0100

    Updates

---
+++ precache-manifest.3e20441af8a9ce440b46fc8ae5ce9236.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e899aa1d0337a4b28c7",
+    "url": "/static/js/4.2e899aa1.chunk.js"
+  },
+  {
+    "revision": "70f722623e0fca2ae7fa",
+    "url": "/static/js/0.70f72262.chunk.js"
+  },
+  {
+    "revision": "6a88085c50400fcc497c",
+    "url": "/static/js/1.6a88085c.chunk.js"
+  },
+  {
+    "revision": "3a4f0102fa88b221a674",
+    "url": "/static/js/runtime~main.3a4f0102.js"
+  },
+  {
+    "revision": "ef16224057b6d633c5a7",
+    "url": "/static/js/main.ef162240.chunk.js"
+  },
+  {
+    "revision": "df5ab0974aa1b03726b8",
+    "url": "/static/js/7.df5ab097.chunk.js"
+  },
+  {
+    "revision": "2d55731d4920078daa52",
+    "url": "/static/js/3.2d55731d.chunk.js"
+  },
+  {
+    "revision": "472226be1783c5ab7db6",
+    "url": "/static/js/6.472226be.chunk.js"
+  },
+  {
+    "revision": "83abfba1d958dd1ae34b",
+    "url": "/static/js/5.83abfba1.chunk.js"
+  },
+  {
+    "revision": "ef16224057b6d633c5a7",
+    "url": "/static/css/main.40697e83.chunk.css"
+  },
+  {
+    "revision": "df5ab0974aa1b03726b8",
+    "url": "/static/css/7.198f854f.chunk.css"
+  },
+  {
+    "revision": "83abfba1d958dd1ae34b",
+    "url": "/static/css/5.8da9df2b.chunk.css"
+  },
+  {
+    "revision": "2e899aa1d0337a4b28c7",
+    "url": "/static/css/4.36c439ea.chunk.css"
+  },
+  {
+    "revision": "2d55731d4920078daa52",
+    "url": "/static/css/3.81dbcc59.chunk.css"
+  },
+  {
+    "revision": "6a88085c50400fcc497c",
+    "url": "/static/css/1.13d42df8.chunk.css"
+  },
+  {
+    "revision": "0ad81203cd3ecc8a76722dff1ce6d851",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd771e3627d45ed0265f940423c7104a.js"
+  "/precache-manifest.3e20441af8a9ce440b46fc8ae5ce9236.js"
 );
 
 workbox.clientsClaim();
